0a9ef7ae802290be76a270d62dde37fed7d50bd6
camilaru15 <mcrueda69848@umanizales.edu.co>
2021-10-29 12:43:03 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import PrivateRoute from './routes/PrivateRoute'
-import './css/App.css';
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import ForgotPassword from './pages/ForgotPassword'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import UpdateProfile from './pages/UpdateProfile'
+import './App.css';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <AuthProvider>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <PrivateRoute path='/dashboard' component={Dashboard} />
-            <PrivateRoute path='/update-profile' component={UpdateProfile} />
-            <Route path='/signup' component={Signup} />
-            <Route path='/login' component={Login} />
-            <Route path='/forgot-password' component={ForgotPassword} />
-          </Switch>
-        </AuthProvider>
-      </Router>
+      <Login/>
     </div>
   );
 }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,73 @@
+import React, {useState } from "react";
+import Label from "./components/Label/labelj"
+import Title from "./components/Title/Title"
+import Input from "./components/Input/Input";
+import './Login.css'
+
+const Login = () => {
+
+    const [ user, setUser] = useState('');
+    const [ password, setPassword] = useState('');
+    const [ passwordError, setpasswordError] = useState(false);
+
+    function handleChange(name,value) {
+        if(name === 'usuario'){
+        setUser(value)
+    } else {
+        if(value.length < 6){
+            setpasswordError(true);
+        }else {
+            setpasswordError(false);
+            setPassword(value)
+        }
+    }
+    };
+
+    function handleSubmit(){
+        let account = { user, password}
+        if(account){
+            console.log('account:', account)
+        }
+    }
+
+    return ( 
+        <div className="login-container">
+            <div className='login-content'>
+                <Title text="Iniciar Sesion"/>
+                <Label text="Usuario"/>
+                <Input
+                attribute={{
+                    id: 'usuario',
+                    name: 'usuario',
+                    type: 'text',
+                    placeholder: 'Ingrese su usuario'
+                }}
+                handleChange={handleChange}
+                />
+                <Label text="Contraseña"/>
+                <Input
+                attribute={{
+                    id: 'contraseña',
+                    name: 'contraseña',
+                    type: 'password',
+                    placeholder: 'Ingrese su contraseña'
+                }}
+                handleChange={handleChange}
+                param={passwordError}
+                />
+                {passwordError &&
+                <label className='label-error'>
+                    Contraseña invalida o incompleta 
+                </label>
+                }
+                <div className='submit-button-container'>
+                <button onClick={handleSubmit} className='submit-button'>
+                    Ingresar
+                </button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Login;
